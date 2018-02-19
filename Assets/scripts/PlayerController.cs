4b4b920f6cf91824f25a36dd900132d64c521df1
john nabil <johnnabil85@gmail.com>
2018-02-19 22:41:07 +0200

    first commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody Body;
+	private float x;
+	private float y=0;
+	private float z;
+	public float speed;
+
+	void Start(){
+		Body = GetComponent<Rigidbody>();
+	}
+
+
+	void FixedUpdate (){
+		x = Input.GetAxis("Horizontal");
+		z = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(x*speed, y, z*speed);
+		Body.AddForce(movement);
+
+	}
+}
